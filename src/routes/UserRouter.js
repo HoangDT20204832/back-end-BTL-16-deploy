c584a23dd787b168382aad8b65fad7c20702b312
Tien Hoang <duongtienhoang2002@gmail.com>
2024-02-05 09:17:27 +0700

    project

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -11,5 +11,5 @@ router.put('/update-user-password/:id', userController.updateUserPassword)
 router.delete('/delete-user/:id',  userController.deleteUser )
 router.get('/get-all-user',  userController.getAllUser )
 router.get('/get-details-user/:id',  authUserMiddleware,userController.getDetailsUser )
-router.post('/refresh-token',userController.refreshToken) //api cung cấp lại access_token mới khi access_token cũ hết hạn mà người dùng ko cần đăng nhập lại
+router.post('/refresh-token',userController.refreshToken) //apiiii cung cấp lại access_token mới khi access_token cũ hết hạn mà người dùng ko cần đăng nhập lại
 module.exports = router
